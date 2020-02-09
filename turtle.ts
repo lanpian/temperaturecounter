ff266f783738a3196f17cd1940dd432712d2eb14
root <77577463@qq.com>
2020-02-09 17:25:16 +0800

    add temperature

--- turtle.ts
+++ turtle.ts
@@ -7,7 +7,7 @@ enum TurtlePenMode {
 /**
  * Turtle graphics blocks
  */
-//% weight=100 color=#0f9c11 icon="\uf188" block="Second Counter"
+//% weight=100 color=#0f9c11 icon="\uf188" block="SecCounter"
 namespace tempcounter {
     let _x: number;
     let _y: number;
